94f5af93954f9a0fb2f76a85631827f38482ba5c
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-05-27 01:05:44 -0400

    Add remainder class for LSC

---
+++ MAIN/remainder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class remainder {
+    public static void rem() throws InterruptedException {
+        @SuppressWarnings("resource")
+        Scanner addput = new Scanner(System.in);
+
+        System.out.printf("\nNUMBER [DIVIDEND]: ");
+        int a = addput.nextInt();
+            
+        System.out.printf("NUMBER [DIVISOR]: ");
+        int b = addput.nextInt();
+
+        int whole = a / b;
+        int remainder =  a % b;
+        System.out.println("WHOLE: " + whole);
+        System.out.println("REMAINDER: " + remainder);
+    }
+}
