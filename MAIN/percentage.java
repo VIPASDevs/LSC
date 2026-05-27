aaf37c1ba3bd0f9cfe7d6407eabe0ddbc532690b
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-05-27 01:05:11 -0400

    Add percentage class for LSC

---
+++ MAIN/percentage.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class percentage {
+    public static void per() throws InterruptedException {
+        @SuppressWarnings("resource")
+        Scanner addput = new Scanner(System.in);
+
+        System.out.printf("\nNUMBER [1]: ");
+        double a = addput.nextInt();
+            
+        System.out.printf("NUMBER [OF]: ");
+        double b = addput.nextInt();
+
+        double percentage = (a / b) % 100;
+        System.out.println(a + " IS " + percentage + "% OF " + b);
+    }
+}
