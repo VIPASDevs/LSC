b5d8500853ec93bb3dce53229d37c18d784f56f0
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-05-26 23:36:37 -0400

    Add division class for LSC

---
+++ MAIN/division.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class division {
+    public static void div() {
+        int total;
+        @SuppressWarnings("resource")
+        Scanner divput = new Scanner(System.in);
+
+        System.out.printf("\nNUMBER [1]: ");
+        int a = divput.nextInt();
+            
+        System.out.printf("NUMBER [2]: ");
+        int b = divput.nextInt();
+
+        total = a / b;
+        System.out.println("TOTAL: " + total);
+    }
+    
+}
