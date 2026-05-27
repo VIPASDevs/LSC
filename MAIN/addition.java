1f78a52c73484b1424695b59d0e28311a2f7c176
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-05-26 23:35:35 -0400

    Add addition class for LSC

---
+++ MAIN/addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class addition {
+    public static void add() throws InterruptedException {
+        int total;
+        @SuppressWarnings("resource")
+        Scanner addput = new Scanner(System.in);
+
+        System.out.printf("\nNUMBER [1]: ");
+        int a = addput.nextInt();
+            
+        System.out.printf("NUMBER [2]: ");
+        int b = addput.nextInt();
+
+        total = a + b;
+        System.out.println("TOTAL: " + total);
+    }
+}
