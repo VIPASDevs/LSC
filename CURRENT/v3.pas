7aab0a675293a8ef7305c8a1fedab42bc3b2a966
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-07-14 02:31:07 -0400

    Added unconditional looping
    Added unconditional looping for better UX.

--- CURRENT/v3.pas
+++ CURRENT/v3.pas
@@ -1,6 +1,6 @@
 program v3;
 
-uses {this declares what other units it uses}
+uses
 ad, subs, tim, di, pow;
 
 var
@@ -8,18 +8,23 @@ input: Char;
 
 begin
 writeln('Lightweight Source Calculator');
-writeln('Version 3.00.00 [FREE PASCAL Vers.]', LineEnding);
-writeln('ADDITION [+] | SUBTRACTION [-] | TIMES [*] | POWER [^]');
+writeln('Vers. Beta 3.00.00 [FREE PASCAL]');
+	
+while True do
+begin
+writeln(LineEnding, 'ADDITION [+], SUBTRACTION [-], TIMES, [*], DIVISION [/], POWER [^], EXIT [.]');
 write('INPUT: ');
 readln(input);
-
+		
 case input of
 '+': AddNum;
 '-': SubNum;
 '*': TimNum;
 '/': DivNum;
 '^': PowNum;
+'.': break;
 else
-writeln('INVALID INPUT.');
+writeln('Invalid input! Please try again.');
 end;
+end; {this is required to end the program. I know it is obv, but it did not click for me for some reason, so I am adding the comment.}
 end.
