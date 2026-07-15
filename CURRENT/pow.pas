290506ef658030b38120dbff8f46474eeedf6410
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-07-15 19:34:26 -0400

    Refactor PowNum procedure with additional uses and delays

--- CURRENT/pow.pas
+++ CURRENT/pow.pas
@@ -3,24 +3,30 @@ unit pow;
 interface
 
 uses
-Math;
+	Math;
 
 procedure PowNum; {makes the code below a proc}
 implementation
 
+uses
+	sysutils;
+
 procedure PowNum;
-var
-a, b, total: Integer;
-{'total: Double' allows power to give the total as an extended number}
+	var
+		a, b, total: Integer;
+		{'total: Double' allows power to give the total as an extended number}
 
-begin
-write(LineEnding, 'BASE NUMBER: ');
-readln(a);
+	begin
+		Sleep(500);
+		write(LineEnding, 'BASE NUMBER: ');
+		readln(a);
 
-write('EXPONENT NUMBER: ');
-readln(b);
+		Sleep(500);
+		write('EXPONENT NUMBER: ');
+		readln(b);
 
-total := (Round(Power(a, b)));
-writeln(LineEnding, 'TOTAL: ', total);
-end;
+		Sleep(300);
+		total := (Round(Power(a, b)));
+		writeln(LineEnding, 'TOTAL: ', total);
+	end;
 end.
