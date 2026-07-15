43f115bfded744411d242e180163236be9370720
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-07-15 19:33:01 -0400

    Refactor input handling and add sleep delays

--- CURRENT/v3.pas
+++ CURRENT/v3.pas
@@ -1,30 +1,34 @@
 program v3;
 
 uses
-ad, subs, tim, di, pow;
+	ad, subs, tim, di, pow, sysutils;
 
 var
-input: Char;
+	input: Char;
 
 begin
-writeln('Lightweight Source Calculator');
-writeln('Vers. 3.00.00 [FREE PASCAL]');
+	Sleep(1000);
+	writeln('Lightweight Source Calculator');
+	Sleep(500);
+	writeln('Vers. 3.00.00 [FREE PASCAL]');
 	
-while True do
-begin
-writeln(LineEnding, 'ADDITION [+], SUBTRACTION [-], TIMES, [*], DIVISION [/], POWER [^], EXIT [.]');
-write('INPUT: ');
-readln(input);
+	while True do
+		begin
+			Sleep(500);
+			writeln(LineEnding, 'ADDITION [+], SUBTRACTION [-], TIMES, [*], DIVISION [/], POWER [^], EXIT [.]');
+			Sleep(100);
+			write('INPUT: ');
+			readln(input);
 		
-case input of
-'+': AddNum;
-'-': SubNum;
-'*': TimNum;
-'/': DivNum;
-'^': PowNum;
-'.': break;
-else
-writeln('Invalid input! Please try again.');
-end;
-end; {this is required to end the program. I know it is obv, but it did not click for me for some reason, so I am adding the comment.}
+			case input of
+				'+': AddNum;
+				'-': SubNum;
+				'*': TimNum;
+				'/': DivNum;
+				'^': PowNum;
+				'.': break;
+			else
+				writeln('Invalid input! Please try again.');
+			end;
+		end; {this is required to end the program. I know it is obv, but it did not click for me for some reason, so I am adding the comment.}
 end.
