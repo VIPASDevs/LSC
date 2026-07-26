50e6c5a133535283d22106f9290c8349423eb18e
VIPAS <voiddevr@protonmail.com>
2026-07-25 22:36:50 -0400

    Create lscde.java

---
+++ EXPERIMENTAL/DIFF_LANG/lscde.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class lscde {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("LSC – GUI-Alpha");
+		
+		JTextField choice = new JTextField(1);
+		
+		JTextField num1 = new JTextField(1);
+		JTextField num2 = new JTextField(1);
+		
+		JButton calculateButton = new JButton("BERECHNEN");
+		
+		JLabel result = new JLabel("ERGEBNIS: ");
+		
+		JLabel options = new JLabel("ADDITION [+], SUBTRAKTION [-], MULTIPLIKATION [*], DIVISION [/]");
+		
+		
+		calculateButton.addActionListener(e -> {
+			String operation = choice.getText();
+			
+			double cnum1 = Double.parseDouble(num1.getText());
+			double cnum2 = Double.parseDouble(num2.getText());
+			double total = 0;
+			
+			if (operation.equals("+")) {
+				total = cnum1 + cnum2;
+				result.setText("GESAMT: " + total);
+			}
+			else if (operation.equals("-")) {
+				total = cnum1 - cnum2;
+				result.setText("GESAMT: " + total);
+			}
+			else if (operation.equals("*")) {
+				total = cnum1 * cnum2;
+				result.setText("GESAMT: " + total);
+			}
+			else if (operation.equals("/")) {
+				total = cnum1 / cnum2;
+				result.setText("GESAMT: " + total);
+			}
+			else {
+				result.setText("GESAMT: НЕПРАВИЛЬНЫЙ");
+			}
+		});
+		
+		frame.setLayout(new GridLayout(6, 1)); //this does top-down not left-right and does 6 rolls, one column
+		
+		
+		frame.add(new JLabel("BETRIEB: "));
+		frame.add(choice);
+		
+		frame.add(new JLabel("NUMMER 1: "));
+		frame.add(num1);
+		
+		frame.add(new JLabel("NUMMER 2: "));
+		frame.add(num2);
+		
+		frame.add(calculateButton);
+		frame.add(result);
+		
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+	}
+}
