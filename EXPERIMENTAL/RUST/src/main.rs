8c39c27b0516c26fb1dceee1b50869a497664df4
VIPAS <voiddevr@protonmail.com>
2026-08-28 01:23:15 -0400

    Implement experimental Rust version
    Decided to learn a little Rust. It is both a godsend and a truckload of horseshit that is more of a bitch than my cat when she doesn't get two bowls of food at once

---
+++ EXPERIMENTAL/RUST/src/main.rs
@@ -0,0 +1,92 @@
+use std::io::{self, Write};
+
+fn main() {
+    let mut total;
+
+    println!("MODERNISED SOURCE CALCULATOR");
+    println!("VERSION BETA");
+    println!("EXIT [.], ADDITION [+], SUBTRACTION [-]");
+    loop {
+        print!("INPUT: ");
+        io::stdout().flush().unwrap();
+
+        let mut finput = String::new();
+        io::stdin()
+            .read_line(&mut finput)
+            .expect("FAILED TO READ");
+
+        let finput: char = finput
+            .trim()
+            .parse()
+            .expect("FAILED TO CONVERT");
+
+        if finput == '+' {
+            print!("\n\nNUMBER: ");
+            io::stdout().flush().unwrap();
+
+            let mut num1 = String::new();
+            io::stdin()
+                .read_line(&mut num1)
+                .expect("FAILED TO READ");
+
+            let num1: i32 = num1
+                .trim()
+                .parse()
+                .expect("FAILED TO CONVERT");
+
+
+            print!("NUMBER: ");
+            io::stdout().flush().unwrap();
+
+            let mut num2 = String::new();
+            io::stdin()
+                .read_line(&mut num2)
+                .expect("FAILED TO READ");
+
+            let num2: i32 = num2
+                .trim()
+                .parse()
+                .expect("FAILED TO CONVERT");
+
+            total = num1 + num2;
+            println!("\nTOTAL: {}\n", total);
+        }
+
+        if finput == '-' {
+            print!("\n\nNUMBER: ");
+            io::stdout().flush().unwrap();
+
+            let mut num1 = String::new();
+            io::stdin()
+                .read_line(&mut num1)
+                .expect("FAILED TO READ");
+
+            let num1: i32 = num1
+                .trim()
+                .parse()
+                .expect("FAILED TO CONVERT");
+
+
+            print!("NUMBER: ");
+            io::stdout().flush().unwrap();
+
+            let mut num2 = String::new();
+            io::stdin()
+                .read_line(&mut num2)
+                .expect("FAILED TO READ");
+
+            let num2: i32 = num2
+                .trim()
+                .parse()
+                .expect("FAILED TO CONVERT");
+
+            total = num1 - num2;
+            println!("\nTOTAL: {}\n", total);
+        }
+
+        if finput == '.' {
+            println!("BYE.");
+            break;
+        }
+    }
+}
