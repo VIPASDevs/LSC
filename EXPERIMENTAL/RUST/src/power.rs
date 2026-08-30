68b52772bd376550b60e5beab2aa059b755a517d
VIPAS <voiddevr@protonmail.com>
2026-08-30 14:42:02 -0400

    Add power function to calculate exponentiation

---
+++ EXPERIMENTAL/RUST/src/power.rs
@@ -0,0 +1,35 @@
+use std::io::{self, Write};
+
+pub fn pow() {
+	print!("\n\nN1: ");
+	io::stdout().flush().unwrap();
+
+		let mut num1 = String::new();
+	io::stdin()
+		.read_line(&mut num1)
+		.expect("FAILED TO PARSE N1.");
+
+	let num1: i32 = num1
+		.trim()
+		.parse()
+		.expect("CONVERSION FOR N1 FAILED.");
+
+
+	print!("N2: ");
+	io::stdout().flush().unwrap();
+
+	let mut num2 = String::new();
+	io::stdin()
+		.read_line(&mut num2)
+		.expect("FAILED TO PARSE N2.");
+
+	let num2: u32 = num2
+		.trim()
+		.parse()
+		.expect("CONVERSION FOR N2 FAILED.");
+
+	let total = num1.pow(num2); //hey I guessed this line right!
+	//but the compiler said it does not need mutable... Why? [RESEARCH REQUIRED]
+	clearscreen::clear().expect("FAILED TO CLEAR SCREEN.");
+	println!("TOTAL: {}\n", total);
+}
