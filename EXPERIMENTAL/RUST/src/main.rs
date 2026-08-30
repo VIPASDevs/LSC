06cacd67ef525484f99fed81a958af314f43f451
VIPAS <voiddevr@protonmail.com>
2026-08-30 14:42:46 -0400

    Enhance calculator with division and power operations
    Added division and power functionalities to the calculator.

--- EXPERIMENTAL/RUST/src/main.rs
+++ EXPERIMENTAL/RUST/src/main.rs
@@ -3,14 +3,18 @@ use addition::add; //to call addition
 mod subtraction; //for subtraction
 use subtraction::sub; //to call sub
 mod multiplication; //to call multiplication
-use multiplication::mul; //to call multiplication
+use multiplication::mul; //for multiplication
+mod division; //to call division
+use division::div; //for division
+mod power;
+use power::pow;
 use std::io::{self, Write};
 
 fn main() {
 	clearscreen::clear().expect("FAILED TO CLEAR SCREEN.");
     println!("Lightweight Source Calculator - RUST VERS.");
     loop {
-		println!("EXIT [.] | ADDITION [+] | SUBTRACTION [-]");
+		println!("EXIT [.] | ADDITION [+] | SUBTRACTION [-] | MULTIPLICATION [*] | DIVISION [/] | POWER [^]");
         print!("INPUT: ");
         io::stdout().flush().unwrap();
 
@@ -34,6 +38,14 @@ fn main() {
 
         else if finput == '*' {
 			mul();
+		}
+
+		else if finput == '/' {
+			div();
+		}
+
+		else if finput == '^' {
+			pow();
 		}
 
        else  if finput == '.' {
